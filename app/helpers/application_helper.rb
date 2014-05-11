fe1455a6356f2eb990e01682b725c5183c7795a8
Ayman Rawashdeh <ayman.rawashdeh@gmail.com>
2014-05-11 10:39:21 +0300

    all

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
 		fields = f.fields_for(association,new_obj,child_index:id) do |builder|
 			render(association.to_s.singularize + "_fields", f: builder)
 		end
-		link_to(name, "#", class: "add-fields", remote: true, data: {id: id, fields: fields.gsub("\n", "")})
+		link_to(name, "#", class: "add-fields",  data: {id: id, fields: fields.gsub("\n", "")})
 	end
 end
