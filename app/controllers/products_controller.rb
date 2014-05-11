a3701ec6b9496c3e152a437da8d8c43e86c621ca
Ayman Rawashdeh <ayman.rawashdeh@gmail.com>
2014-05-11 13:28:25 +0300

    with multistep functinality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,20 +18,30 @@ class ProductsController < ApplicationController
   end
 
   def create
-    puts product_params
     session[:products_params].deep_merge!(without_images) if params[:product]
-    puts session[:products_session]
-  	
-    #@product = Product.new(product_params)
-  	#puts product_params
-  	#@product.save!
-  	#redirect_to products_path
+
+    submitted_hash = {}
+    submitted_hash = product_params
+    submitted_hash.deep_merge!(session[:products_params])
+
+    pics = product_params[:pictures_attributes].values
+    pics.each do |p|
+      pid = p["img"]
+      unless pid.blank?
+        preloaded = Cloudinary::PreloadedFile.new(pid)
+        PictureTemp.where(public_id: preloaded.public_id).first.destroy
+      end
+    end
+
+    @product = Product.new(product_params)
+  	@product.save!
+  	redirect_to products_path
 
   end
 
   def without_images
     car_params_without_pictures = product_params
-    car_params_without_pictures.delete(:pictures_attributes) if params[:car][:pictures_attributes]
+    car_params_without_pictures.delete(:pictures_attributes) if params[:product][:pictures_attributes]
     car_params_without_pictures
   end
 
